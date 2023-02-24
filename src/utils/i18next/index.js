ea0f59fb8b174322965237c8017cb773680d7048
VadymStratiienko <VadymStratiienko44@gmail.com>
2023-02-24 12:14:01 +0200

    fix trln

--- src/utils/i18next/index.js
+++ src/utils/i18next/index.js
@@ -2,24 +2,13 @@ import i18n from 'i18next';
 import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import ua from '../../common/locales/ua/translation.json';
-import en from '../../common/locales/en/translation.json';
 
 i18n
   .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: {
-        translation: en,
-      },
-      ua: {
-        translation: ua,
-      },
-    },
-    fallbackLng: 'en',
-    debug: false,
+    fallbackLng: 'ua',
     detection: {
       order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
       caches: ['cookie'],
